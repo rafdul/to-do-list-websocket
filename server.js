fcf7c249d1850d005a1737462cc895c78cc30af7
rafdul <rafalduluk@gmail.com>
2021-03-18 14:14:09 +0100

    Init project

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found...' });
+});
+
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port: 8000');
+});
+
+const io = socket(server);
