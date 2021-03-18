0b9ba0f53e70fcefdb099e7d096cb162bf6b3885
rafdul <rafalduluk@gmail.com>
2021-03-18 23:58:02 +0100

    Add next methods: addTasks, submitForm, updatedTasks

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
 import io from 'socket.io-client';
+import uniqid from 'uniqid';
 
 class App extends React.Component {
   state = {
     // tasks: [{id: 1, name: 'aa'}, {id: 2, name: 'bb'}],
     tasks: [],
-  }
-
-  componentDidMount(){
-    // this.socket.connect('localhost:8000');
-    this.socket = io.connect('localhost:8000');
+    taskName: '',
   };
 
-  removeTask(id) {
-    this.setState({ tasks: this.state.tasks.splice(id, 1) });
-    this.socket.emit('removeTask', id);
+  componentDidMount(){
+    this.socket = io.connect('http://localhost:8000/');
+    this.socket.on('addTask', (message) => this.addTask(message));
+    this.socket.on('removeTask', (id) => this.removeTask(id));
+    this.socket.on('updateData', (tasks) => this.updateTasks(tasks));
+  };
+
+  removeTask(id, local) {
+    this.setState({ tasks: this.state.tasks.filter(task => task.id !== id) });
+    if(local === true) {
+      this.socket.emit('removeTask', id);
+    }
+  };
+
+  addTask(task) {
+    console.log('addTask task:', task);
+    this.setState({ tasks: [...this.state.tasks, task]});
+  };
+
+  submitForm(event) {
+    event.preventDefault();
+    const newTask = { id: uniqid(), name: this.state.taskName };
+    this.addTask(newTask);
+    console.log('newTask in submitForm:', newTask);
+    this.socket.emit('addTask', newTask);
+    this.setState({ taskName: '' });
+  };
+
+  updateTasks(tasks) {
+    this.setState({ tasks: tasks });
   };
 
   render() {
-    const { tasks } = this.state;
+    const { tasks, taskName } = this.state;
 
-    
+    console.log('tasks', tasks);
+    console.log('taskName', taskName);
 
     return (
       <div className="App">
@@ -33,21 +58,26 @@ class App extends React.Component {
 
           <ul className="tasks-section__list" id="tasks-list">
             {tasks.map(task => (
-              <li key={task.id} class="task">
+              <li key={task.id} className="task">
                 {task.name} 
-                <button class="btn btn--red" onClick={() => this.removeTask(task.id)}>
+                <button className="btn btn--red" onClick={() => this.removeTask(task.id, true)}>
                   Remove
                 </button>
               </li>
             ))}
-
-            {/* <li class="task">Shopping <button class="btn btn--red">Remove</button></li>
-            <li class="task">Go out with a dog <button class="btn btn--red">Remove</button></li> */}
           </ul>
 
-          <form id="add-task-form">
-            <input className="text-input" autocomplete="off" type="text" placeholder="Type your description" id="task-name" />
-            <button className="btn" type="submit">Add</button>
+          <form id="add-task-form" onSubmit={(submit) => this.submitForm(submit)}>
+            <input 
+              className="text-input" 
+              autoComplete="off" 
+              type="text" 
+              placeholder="Type your description" 
+              id="task-name" 
+              value={taskName}
+              onChange={(event) => this.setState({ taskName: event.target.value })}
+            />
+            <button className="btn" type="submit" >Add</button>
           </form>
 
         </section>
--- server.js
+++ server.js
@@ -1,34 +1,39 @@
 const express = require('express');
 const socket = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 
 const tasks = [];
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found...' });
-});
+app.use(cors());
 
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
 
-const io = socket(server);
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found...' });
+});
 
-io.on = ('connection', (socket) => {
+const io = socket(server, { cors: { origin: '*', } });
+// const io = socket(server);
+
+
+io.on('connection', (socket) => {
   console.log('New client - its id ' + socket.id);
   socket.on('updateDate', () => {
     console.log('Wysłanie tablicy tasks do klienta')
     io.to(socket.id).emit('updateDate', tasks);
   });
-  socket.on('addTask', (message) => {
-    console.log('serv nasłuchuje addTask, message:', message);
-    tasks.push(message);
-    socket.broadcast.emit('addTask', message);
+  socket.on('addTask', (newTask) => {
+    console.log('serv nasłuchuje addTask, message:', newTask);
+    tasks.push(newTask);
+    socket.broadcast.emit('addTask', newTask);
   });
-  socket.on('removeTask', () => {
-    const index = tasks.indexOf(index);
+  socket.on('removeTask', (id) => {
+    const index = tasks.indexOf(tasks.find(task => task.id === id));
     tasks.splice(index, 1);
-    socket.broadcast.emit('removeTask', index);
+    socket.broadcast.emit('removeTask', id);
   });
 });
